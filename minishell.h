e4980fc8c961ba885e16bb6d9e82a4291423b1d0
shinji-japaaaan <84433138+shinji-japaaaan@users.noreply.github.com>
2025-01-11 07:03:56 +0900

    Create minishell.h

---
+++ minishell.h
@@ -0,0 +1,16 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <signal.h>
+
+// 共通の構造体
+
+
+// 関数プロトタイプ
+
+
+#endif
