f5b76d5216cf8b012014dbe96b8b766dfbabc837
sishizaw <shinji.japaaan@gmail.com>
2025-01-11 11:38:18 +0900

    他の内部コマンドも実装した

--- src/executor/builtins.c
+++ src/executor/builtins.c
@@ -6,7 +6,7 @@
 /*   By: sishizaw <sishizaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 10:40:24 by sishizaw          #+#    #+#             */
-/*   Updated: 2025/01/11 11:07:19 by sishizaw         ###   ########.fr       */
+/*   Updated: 2025/01/11 11:16:59 by sishizaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,42 +15,56 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+extern char **environ; // 環境変数へのアクセス
+
 void change_directory(char *path) {
-    if (strcmp(path, "~") == 0) {
-        // "~"が指定された場合はHOMEディレクトリに変更
-        char *home = getenv("HOME");
-        if (home != NULL) {
-            if (chdir(home) == -1) {
-                perror("cd failed");
-            } else {
-                char cwd[256];
-                if (getcwd(cwd, sizeof(cwd)) != NULL) {
-                    printf("Current directory: %s\n", cwd);
-                } else {
-                    perror("getcwd failed");
-                }
-            }
-        } else {
-            perror("HOME environment variable is not set");
-        }
-    } else {
-        // 他のディレクトリに移動
-        if (chdir(path) == -1) {
-            perror("cd failed");
-        } else {
-            char cwd[256];
-            if (getcwd(cwd, sizeof(cwd)) != NULL) {
-                printf("Current directory: %s\n", cwd);
-            } else {
-                perror("getcwd failed");
-            }
-        }
+    if (chdir(path) == -1) {
+        perror("cd failed");
     }
 }
 
 void exit_shell() {
     printf("Exiting shell...\n");
     exit(0);
+}
+
+void echo_command(char *args) {
+    if (args) {
+        printf("%s\n", args);
+    } else {
+        printf("\n");
+    }
+}
+
+void print_working_directory() {
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("%s\n", cwd);
+    } else {
+        perror("pwd failed");
+    }
+}
+
+void print_environment() {
+    for (char **env = environ; *env; ++env) {
+        printf("%s\n", *env);
+    }
+}
+
+void export_variable(char *arg) {
+    if (!arg || !strchr(arg, '=')) {
+        fprintf(stderr, "export: invalid argument\n");
+        return;
+    }
+    if (putenv(arg) != 0) {
+        perror("export failed");
+    }
+}
+
+void unset_variable(char *arg) {
+    if (unsetenv(arg) != 0) {
+        perror("unset failed");
+    }
 }
 
 int main() {
@@ -62,9 +76,19 @@ int main() {
         input[strcspn(input, "\n")] = '\0'; // 改行を除去
 
         if (strncmp(input, "cd ", 3) == 0) {
-            change_directory(input + 3); // "cd "の後のパスを渡す
+            change_directory(input + 3);
         } else if (strcmp(input, "exit") == 0) {
             exit_shell();
+        } else if (strncmp(input, "echo ", 5) == 0) {
+            echo_command(input + 5);
+        } else if (strcmp(input, "pwd") == 0) {
+            print_working_directory();
+        } else if (strcmp(input, "env") == 0) {
+            print_environment();
+        } else if (strncmp(input, "export ", 7) == 0) {
+            export_variable(input + 7);
+        } else if (strncmp(input, "unset ", 6) == 0) {
+            unset_variable(input + 6);
         } else {
             printf("Unsupported command: %s\n", input);
         }
@@ -72,6 +96,7 @@ int main() {
 
     return 0;
 }
+
 
 
 
